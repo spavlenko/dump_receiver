83ce5dc411ed2dfabf5159acf4181d9e51cac59b
sergeypavlenko <sir.wolond@gmail.com>
2020-06-25 12:31:14 +0300

    removed debug code

--- controllers/register_event.go
+++ controllers/register_event.go
@@ -1,25 +1,16 @@
 package controllers
 
 import (
-	"bytes"
 	"database/sql"
 	"dump_reciever/models"
 	"dump_reciever/types"
 	"fmt"
 	"github.com/labstack/echo"
-	"io/ioutil"
 	"net/http"
 )
 
 func RegisterEvent(database *sql.DB) echo.HandlerFunc {
 	return func(c echo.Context) error {
-
-		var bodyBytes []byte
-		if c.Request().Body != nil {
-			bodyBytes, _ = ioutil.ReadAll(c.Request().Body)
-		}
-		// Restore the io.ReadCloser to its original state
-		c.Request().Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
 		var eventInfo types.EventInfo
 		er := c.Bind(&eventInfo) // bind the structure with the context body
 		// or no panic!
